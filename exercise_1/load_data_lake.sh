62a5738cad3980f9275a5db614fbe563a5bffe30
Sombiri Enwemeka <sombiri@enwemeka.com>
2017-06-26 04:14:38 -0700

    Made data loading script atomic
    - Added wget for data zip so seperate download is not required
    - Added checks for command success
        - Perform cleanup on any failures
    - Colorized error and warning messages
    - Minor code cleanup

--- exercise_1/load_data_lake.sh
+++ exercise_1/load_data_lake.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
 
-# Working vars, can also pass positional args to replace
-# Defaults assume script is called from the directory containing
-# the unzipped hospital data directory.
+# Working vars, can also pass positional args to replace defaults
+
 work_dir=${1:-Hospital_Revised_Flatfiles}
 o_dir=${2:-"processed_files"}
 hdfs_path=${3:-"/user/w205/hospital_compare"}
 start_dir=$(pwd)
+RED='\x1B[1;31m'
+GRN='\x1B[1;32m'
+BLU='\x1B[1;34m'
+NC='\x1B[0m' # No Color
 
 echo
-echo "INPUT DIR: "$work_dir
-echo "OUTPUT DIR: "$work_dir"/"$o_dir
+echo -e "${BLU}[INFO]${NC}: INPUT DIR: ${work_dir}"
+echo -e "${BLU}[INFO]${NC}: OUTPUT DIR: ${work_dir}/${o_dir}"
 
-#su - w205
-cd ./$work_dir
+wget_opts="-O${work_dir}.zip"
+
+data_url="https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H\
+1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&fil\
+ename=Hospital_Revised_Flatfiles.zip"
+
+# Download and unzip data files
+if ! wget "${wget_opts}" "${data_url}"}; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Unable to download data."
+  exit 1
+fi
+
+if ! rm -rf "${work_dir}"; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Could not access working directory."
+  exit 1
+fi
+
+if ! mkdir "${work_dir}"; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Could not create working directory: ${work_dir}."
+  exit 1
+fi
+
+if ! unzip "Hospital_Revised_Flatfiles.zip" "-d" "${work_dir}"; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Unable to unzip download data."
+  exit 1
+fi
+
+if ! cd "./${work_dir}"; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Unable to cd to working directory."
+  rm -rf "${work_dir}"
+  rm -f "${work_dir}.zip"
+  exit 1
+fi
 
 echo
-echo "Renaming hospital data files and stripping CSV headers"
+echo -e "${BLU}[INFO]${NC}: Renaming hospital data files and stripping CSV headers"
 
 fn1="Hospital*General*Information"
 fn2="Timely*and*Effective*Care"
@@ -31,7 +70,7 @@ done | sh
 
 # Create output directory
 if [ ! -d "$o_dir" ]; then
-	mkdir ${o_dir}
+	mkdir "${o_dir}"
 fi
 
 # Simplify filenames and strip CSV headers
@@ -53,14 +92,31 @@ done | sh
 
 
 # Add output files to HDFS
-cd ./$o_dir
+if ! cd "./${o_dir}"; then
+  echo
+  echo -e "${RED}[ERROR]${NC}: Unable to cd to working directory."
+  rm -rf "${work_dir}"
+  rm -f "${work_dir}.zip"
+  exit 1
+fi
 
-hdfs dfs -mkdir ${hdfs_path}
+echo
+echo -e "${BLU}[INFO]${NC}: Initializing HDFS path ${hdfs_path}"
+echo -e "${BLU}[INFO]${NC}: Hang tight, it's still working."
+hdfs dfs -rm -R "${hdfs_path}"
+hdfs dfs -mkdir "${hdfs_path}"
 
+echo
+echo -e "${BLU}[INFO]${NC}: Adding csv files to HDFS path"
 for filename in *.csv; do
-	hdfs dfs -mkdir ${hdfs_path}/${filename//.csv/}
-	hdfs dfs -put ${filename} ${hdfs_path}/${filename//.csv/}
+	hdfs dfs -mkdir "${hdfs_path}/${filename//.csv/}"
+	hdfs dfs -put "${filename}" "${hdfs_path}/${filename//.csv/}"
 done | sh
 
 # Show contents of directory after completion
-hdfs dfs -ls ${hdfs_path}
+echo
+echo -e "${BLU}[INFO]${NC}: HDFS Path contents"
+hdfs dfs -ls -R "${hdfs_path}"
+echo
+echo -e "${BLU}[INFO]${NC}: ${GRN}SUCCESS!!${NC}"
+exit 0
